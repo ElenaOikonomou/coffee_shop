741c04c1a9eb157b8634decd6d86a44d327f3884
Eleni Oikonomou <oikonomou.g.eleni@outlook.com>
2024-10-25 18:28:44 -0700

    menu_ comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import React from 'react';
+import Navbar from './Components/Navbar';
 
 export default function App() {
   return (
     <div className="App">
+    <Navbar/>
       <header className="App-header">
         
         <p>
